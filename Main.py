671b58363994204ed4d24e44a66685575574f8e8
Dhruv-Srivastava-9 <105653954+Dhruv-Srivastava-9@users.noreply.github.com>
2023-08-31 23:40:37 +0530

    Added Selector Functionality
    Added the class and function to call out different modes, added function for generating the word for easier use.
    Test Run#1 to check, if it works or not

--- Main.py
+++ Main.py
@@ -1,1 +1,27 @@
+"""Imports
+-Random
+"""
+import Random
 
+Class Hangman:
+  def selector(self,mode): 
+    easy = False
+    medium = False
+    hard = False
+    if mode == 'e':
+      self.easy()
+      easy = True
+    elif mode == 'm':
+      self.medium()
+      medium = True
+    elif mode == 'h':
+      self.hard()
+      hard = True
+  def word_generator(self): #Add functanality of changing the length of the word according to the input of selector
+    pass
+  def easy(self): #Add the word generated from word_generator
+    return "This is easy mode"
+  def medium(self): #Add the word generated from word_generator
+    return "This is Medium mode"
+  def hard(self): #Add the word generated from word_generator
+    return "This is Hard mode"
