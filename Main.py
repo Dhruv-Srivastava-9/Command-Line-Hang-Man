24995af6efd24479220b8aeccb4b6874a240d767
Dhruv-Srivastava-9 <105653954+Dhruv-Srivastava-9@users.noreply.github.com>
2023-09-01 00:46:02 +0530

    Added Functanality of Word Generated Test
    Test 2# Not Passed

--- Main.py
+++ Main.py
@@ -3,10 +3,20 @@
 """
 
 class Hangman:
-  def selector(self,mode): 
+  def check(self, running):
+    #If True then run the word_generator, if false break
+    while running == True:
+      word = self.word_generator()
+  def selector(self,mode):
     easy = False
     medium = False
     hard = False
+    print(f"Mode is {mode}")
+    self.running = True
+    while True:
+      self.check(self.running)
+      break
+
     if mode == 'e':
       self.easy()
       easy = True
@@ -17,8 +27,15 @@ class Hangman:
       self.hard()
       hard = True
   def word_generator(self): #Add functanality of changing the length of the word according to the input of selector
-    pass
-  def easy(self): #Add the word generated from word_generator
+    mode = self.mode
+    if mode == 'e':
+      print("3 letter string")#Write code to generate for easy one
+    elif mode == 'm':
+      print("5 letter string") #Wrie code to generate for medium one
+    elif mode == 'h':
+      print("7 letter string") #Write code to generate for hard one
+
+  def easy(self,): #Add the word generated from word_generator
     print("This is easy mode")
   def medium(self): #Add the word generated from word_generator
     print("This is Medium mode")
