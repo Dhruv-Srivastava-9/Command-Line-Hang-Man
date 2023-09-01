091fe1b6a49e8b9456e083344a4d94cf45210a0e
Dhruv-Srivastava-9 <105653954+Dhruv-Srivastava-9@users.noreply.github.com>
2023-09-01 19:26:31 +0530

    Added more functanality
    -Addd Word_generator, word_hider and also worked on easy lil bit.

--- Main.py
+++ Main.py
@@ -1,49 +1,77 @@
 """Imports
 -Random
+-String
 """
+import random
+import string
 
 class Hangman:
-  def check(self, running):
-    #If True then run the word_generator, if false break
-    while running == True:
-      word = self.word_generator()
   def selector(self,mode):
-    easy = False
-    medium = False
-    hard = False
-    print(f"Mode is {mode}")
-    self.running = True
-    while True:
-      self.check(self.running) #Issue here
-      break
 
+    modes_list = ["e for Easy","m for Medium","h for Hard"]
+    print(modes_list)
+    
+    self.running = True
     if mode == 'e':
-      self.easy()
-      easy = True
+      self.easy(self.word)
+      
+      y = "Easy"
     elif mode == 'm':
       self.medium()
-      medium = True
+      
+      y = "Medium"
     elif mode == 'h':
       self.hard()
-      hard = True
-  def word_generator(self): #Add functanality of changing the length of the word according to the input of selector
-    mode = self.mode
-    if mode == 'e':
-      print("3 letter string")#Write code to generate for easy one
-    elif mode == 'm':
-      print("5 letter string") #Wrie code to generate for medium one
-    elif mode == 'h':
-      print("7 letter string") #Write code to generate for hard one
+      
+      y = "Hard"
+    else:
+      print("Invalid Input")
+      y = "Invalid"
+    print(f"Mode is{y}")
+    self.mode = mode
+  def word_generator(self):
+    letters = string.ascii_lowercase
+    if self.mode == 'e':
+        self.word = ''.join(random.choice(letters) for _ in range(3))
+    elif self.mode == 'm':
+        self.word = ''.join(random.choice(letters) for _ in range(5))
+    elif self.mode == 'h':
+        self.word = ''.join(random.choice(letters) for _ in range(7))
 
-  def easy(self,): #Add the word generated from word_generator
-    print("This is easy mode")
-  def medium(self): #Add the word generated from word_generator
+  def word_hider(word):
+    hiddden = word
+    for i in range(len(word)):
+      hiddden[i]='_'
+
+    
+  def word_checker(self):
+    pass
+  def easy(self,word):
+    guesses = 5
+    errors = 0
+    correct = 3
+    print("\nEasy Mode Prompt")
+    print(f"\nYou got {guesses} Guesses")
+    print(f"\nYou have to guess {correct} letters")
+
+    print(word) #Test
+    guess = input()
+    if not guess.isalnum:
+      print("Nice Try Bud, but no numbers are there")
+    elif len(guess) >1:
+      print("Hey 1 character at a Time")
+
+  def medium(self): 
     print("This is Medium mode")
-  def hard(self): #Add the word generated from word_generator
+  def hard(self):
     print("This is Hard mode")
 
-
+#Creating Instance
 hangman = Hangman()
 m = input()
-hangman.selector(m)
+if not m.isalpha:
+  print("Bro, only strings")
+else:
+  hangman.word_generator()
+  hangman.selector(m)
 
