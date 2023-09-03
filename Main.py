0bec36740b07dfeeb3c42fa18ecdd86461b93975
Dhruv-Srivastava-9 <105653954+Dhruv-Srivastava-9@users.noreply.github.com>
2023-09-03 11:43:38 +0530

    Completed
    -Added Functnality for All of the game modes
    -Lazy to fix the exploit

--- Main.py
+++ Main.py
@@ -16,11 +16,11 @@ class Hangman:
       
       
     elif mode == 'm':
-      self.medium()
+      self.medium(self.word_generator(mode))
       
       
     elif mode == 'h':
-      self.hard()
+      self.hard(self.word_generator(mode))
       
       
     else:
@@ -61,13 +61,16 @@ class Hangman:
     print(f"\nYou have to guess {correct} letters")
     hid = self.word_hider2(self.word)
     print(f"\nThe word is: {hid}")
-    print(self.word) #Test
+    print(self.word)
     while correct > 0:
       guess = input()
       if not guess.isalnum:
         print("Nice Try Bud, but no numbers are there")
       elif len(guess) >1:
         print("Hey 1 character at a Time")
+      elif len(guess) == 0:
+        print('Atleast give an input...')
+        print(hid)
       else:
         if guess in lil:
           index = lil.index(guess)
@@ -88,6 +91,7 @@ class Hangman:
           errors = errors + 1
           guesses = guesses-1
           print(f"\nGuesses left are: {guesses}")
+          print(hid)
           if errors == 3:
             print("\nGame Over")
             print(f"You guessed {guessed} letters")
@@ -95,10 +99,101 @@ class Hangman:
             break
 
 
-  def medium(self): 
-    print("This is Medium mode")
+  def medium(self,word): 
+    guesses = 3
+    errors = 0
+    correct = 5
+    guessed = 0
+    lil = list(self.word)
+    print("\Medium Mode Prompt")
+    print(f"\nYou got {guesses} Guesses")
+    print(f"\nYou have to guess {correct} letters")
+    hid = self.word_hider2(self.word)
+    print(f"\nThe word is: {hid}")
+    print(self.word)
+    while correct > 0:
+      guess = input()
+      if not guess.isalnum:
+        print("Nice Try Bud, but no numbers are there")
+      elif len(guess) >1:
+        print("Hey 1 character at a Time")
+      elif len(guess) == 0:
+        print('Atleast give an input...')
+        print(hid)
+      else:
+        if guess in lil:
+          index = lil.index(guess)
+          hid[index] = guess
+          print(f"Correct Guess, {hid}")
+          guesses = guesses-1
+          correct = correct-1
+          guessed = guessed + 1
+          
+        if guessed == 5:
+          print("\nGG, You did")
+          print(f"\nYou guessed {guessed} letters!")
+          print("\nBack to the selection screen...")
+          break
+
+        elif not guess in lil:
+          print("Wrong Guess")
+          errors = errors + 1
+          guesses = guesses-1
+          print(f"\nGuesses left are: {guesses}")
+          print(hid)
+          if errors == 3:
+            print("\nGame Over")
+            print(f"You guessed {guessed} letters")
+            print(f"The word was {self.word}")
+            break
+
   def hard(self):
-    print("This is Hard mode")
+    guesses = 3
+    errors = 0
+    correct = 7
+    guessed = 0
+    lil = list(self.word)
+    print("\Medium Mode Prompt")
+    print(f"\nYou got {guesses} Guesses")
+    print(f"\nYou have to guess {correct} letters")
+    hid = self.word_hider2(self.word)
+    print(f"\nThe word is: {hid}")
+    print(self.word)
+    while correct > 0:
+      guess = input()
+      if not guess.isalnum:
+        print("Nice Try Bud, but no numbers are there")
+      elif len(guess) >1:
+        print("Hey 1 character at a Time")
+      elif len(guess) == 0:
+        print('Atleast give an input...')
+        print(hid)
+      else:
+        if guess in lil:
+          index = lil.index(guess)
+          hid[index] = guess
+          print(f"Correct Guess, {hid}")
+          guesses = guesses-1
+          correct = correct-1
+          guessed = guessed + 1
+          
+        if guessed == 7:
+          print("\nGG, You did")
+          print(f"\nYou guessed {guessed} letters!")
+          print("\nBack to the selection screen...")
+          break
+
+        elif not guess in lil:
+          print("Wrong Guess")
+          errors = errors + 1
+          guesses = guesses-1
+          print(f"\nGuesses left are: {guesses}")
+          print(hid)
+          if errors == 3:
+            print("\nGame Over")
+            print(f"You guessed {guessed} letters")
+            print(f"The word was {self.word}")
+            break
 
 #Creating Instance
 hangman = Hangman()
